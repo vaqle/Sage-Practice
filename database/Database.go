3e90071391d44e4d45875d3e9279cf3faa454551
vaqle <92319390+vaqle@users.noreply.github.com>
2022-05-11 17:15:32 -0400

    Update Database.go

--- database/Database.go
+++ database/Database.go
@@ -3,8 +3,13 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"github.com/df-mc/dragonfly/server"
 	"github.com/go-sql-driver/mysql"
+	"go.uber.org/atomic"
+	"time"
 )
+
+var Srv *server.Server
 
 func Main() {
 	initTables()
@@ -13,6 +18,7 @@ func Main() {
 var DATABASE *sql.DB
 
 func initTables() {
+	update()
 	db, err := sql.Open("mysql", "db_383699:58a8761227@tcp(na02-db.cus.mc-panel.net:3306)/db_383699")
 	if err != nil {
 		panic(err)
@@ -25,6 +31,17 @@ func initTables() {
 		return
 	}
 	fmt.Println("Tables created")
+}
+
+var Players atomic.Int32
+//so to @prim learned what atomics are
+func update() {
+	go func() {
+		for {
+			Players.Store(int32(len(Srv.Players())))
+			time.Sleep(1 * time.Second)
+		}
+	}()
 }
 
 func GetDatabase() *sql.DB {
