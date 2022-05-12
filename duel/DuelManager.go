fb3e89c95d4853e7a8f5e95e0737b105d009215f
vaqle <92319390+vaqle@users.noreply.github.com>
2022-05-11 22:11:43 -0400

    Update DuelManager.go

--- duel/DuelManager.go
+++ duel/DuelManager.go
@@ -1,7 +1,55 @@
 package duel
-//TODO USE MUTEXES FOR DUELS add() destroy() get()
-var duels = make(map[string]*DuelMatch)
 
-func getDuels() map[string]*DuelMatch {
-	return duels
+import (
+	"github.com/df-mc/dragonfly/server/player"
+	"sync"
+)
+
+type Matches struct {
+	mList map[string]*Duel
+	mutex sync.Mutex
+}
+
+var matches = Matches{mList: make(map[string]*Duel)}
+
+func (m *Matches) register(d *Duel) *Duel {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+	m.mList[d.UUID] = d
+	return d
+}
+
+func (m *Matches) unregister(d *Duel) {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+	delete(m.mList, d.UUID)
+}
+
+func GetMatch(p *player.Player) *Duel {
+	for _, d := range matches.mList {
+		for _, p2 := range d.getDuelPlayers() {
+			if len(d.getPlayers()) != 0 {
+				if p2.Name() == p.Name() {
+					return d
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func StartMatch(p1 *player.Player, p2 *player.Player) {
+	d := &Duel{
+		players: []*player.Player{p1, p2}, UUID: "HEY",
+		running: true, duration: 0,
+	}
+	d.addPlayer(p1)
+	d.addPlayer(p2)
+	d.tick()
+	matches.register(d)
+
+}
+
+func (d *Duel) stop() {
+	matches.unregister(d)
 }
