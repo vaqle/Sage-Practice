9cf09678fa7dc3b4f22a134d319eee68ace00515
vaqle <92319390+vaqle@users.noreply.github.com>
2022-05-11 22:10:40 -0400

    Update Session.go

--- sessions/Session.go
+++ sessions/Session.go
@@ -1,14 +1,17 @@
 package sessions
 
 import (
+	"Df_while_go/database"
+	"Df_while_go/duel"
+	"Df_while_go/queue"
+	"database/sql"
 	"fmt"
 	"github.com/df-mc/dragonfly/server/item"
 	"github.com/df-mc/dragonfly/server/player"
 	"github.com/df-mc/dragonfly/server/player/scoreboard"
+	"strconv"
 	"time"
 )
-import "Df_while_go/database"
-import "database/sql"
 
 type Session struct {
 	player *player.Player
@@ -67,15 +70,23 @@ func (s *Session) AddKills() {
 }
 
 func (s *Session) UpdateScoreboard() {
-	sb := scoreboard.New("§6§lSage")
-	sb.RemovePadding()
-	ticker := time.NewTicker(time.Second * 2)
-	for _ = range ticker.C {
+
+	ticker := time.NewTicker(time.Second * 1)
+	for range ticker.C {
 		if s.player == nil {
 			ticker.Stop()
 			return
 		}
+		s.score()
+		matchmaking(s.player)
 
+	}
+}
+
+func (s *Session) score() {
+	sb := scoreboard.New("§6§lSage")
+	sb.RemovePadding()
+	if duel.GetMatch(s.player) == nil {
 		sb.Set(0, "\uE000")
 		sb.Set(1, " §r§fOnline: §6"+database.Players.String())
 		sb.Set(2, " §r§fMatches: §6"+"0")
@@ -83,6 +94,31 @@ func (s *Session) UpdateScoreboard() {
 		sb.Set(4, " §r§6sagehcf.club")
 		sb.Set(5, "§e\uE000")
 		s.player.SendScoreboard(sb)
+	} else {
+		duelr := duel.GetMatch(s.player)
+		opponet := duelr.GetOpponet(s.player)
+		duration := duelr.GetDuration()
+		sb.Set(0, "\uE000")
+		sb.Set(1, " §r§fOpponet: §6"+opponet.Name())
+		sb.Set(2, " §r§fDuration: §6"+strconv.Itoa(duration))
+		sb.Set(3, "\n")
+		sb.Set(4, " §r§6sagehcf.club")
+		sb.Set(5, "§e\uE000")
+		s.player.SendScoreboard(sb)
+	}
+}
+
+func matchmaking(p *player.Player) {
+	for _, q := range queue.Queued {
+		if queue.IsQueued(p) {
+			if q.Player.Name() == p.Name() {
+				return
+			}
+			p.Message("Found player in queue")
+			queue.RemoveFromQueue(p)
+			queue.RemoveFromQueue(q.Player)
+			duel.StartMatch(p, q.Player)
+		}
 	}
 }
 
